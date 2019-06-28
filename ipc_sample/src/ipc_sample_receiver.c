4060a856ff34b5688a6026b6f8652aa53e2f2949
Timo Nicolai <timonicolai@arcor.de>
2019-06-28 20:30:11 +0200

    Unsuccessfully try binding receiving cap to thread

--- ipc_sample/src/ipc_sample_receiver.c
+++ ipc_sample/src/ipc_sample_receiver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <l4/re/env.h>
+#include <l4/sys/rcv_endpoint.h>
 #include <l4/sys/utcb.h>
 #include <l4/sys/types.h>
 
@@ -12,6 +14,10 @@ int main(int argc, char **argv)
   l4_cap_idx_t chan_cap = get_chan_cap(argc, argv);
 
   printf("Hello from receiver\n");
+
+  l4re_env_t *env = l4re_env();
+  if (l4_msgtag_has_error(l4_rcv_ep_bind_thread(chan_cap, env->main_thread, 0)))
+    fprintf(stderr, "failed to bind capability to main thread\n");
 
   /* receive IPC */
   l4_msgtag_t tag = l4_ipc_receive(chan_cap, l4_utcb(), L4_IPC_NEVER);
